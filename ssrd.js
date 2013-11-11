3f17a1053b6bf52d325111830762099e018c962f
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-11-11 20:02:20 +0800

    Update ssrd structure.

--- ssrd.js
+++ ssrd.js
@@ -1,8 +1,45 @@
-
+var redis = require( "redis" );
 
 /*
 	ssrd
 		"squared" or save, sync, read and delete, the basic
 			operations for manipulating redis data storage.
-			
+
 */
+
+
+var saveData = function saveData( ){
+
+};
+
+var syncData = function syncData( ){
+
+};
+
+var readData = function readData( ){
+
+};
+
+var deleteData = function deleteData( ){
+
+};
+
+var checkData = function checkData( ){
+
+};
+
+var saveMultipleData = function saveMultipleData( ){
+
+};
+
+var syncMultipleData = function syncMultipleData( ){
+
+};
+
+var readMultipleData = function readMultipleData( ){
+
+};
+
+var deleteMultipleData = function deleteMultipleData( ){
+
+};
